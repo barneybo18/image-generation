0613606e241806cbe80e72d1d87429ffda34c90a
banny2005 <barneybo2005@gmail.com>
2023-10-12 04:44:22 +0100

    update client7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { Home, CreatePost } from './pages'
  const App = () => {
   return (
         <BrowserRouter>
-          <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] fixed'>
+          <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] fixed z-[9999]'>
             <Link to="/">
               <img src={logo} alt="logo" className='w-[180px] object-contain' />
             </Link>
