e90ca3d7849c4c1ef5c238e42b9886c7f928ce24
banny2005 <barneybo2005@gmail.com>
2023-10-12 04:31:54 +0100

    update client4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { Home, CreatePost } from './pages'
  const App = () => {
   return (
         <BrowserRouter>
-          <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] sticky top-0'>
+          <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
             <Link to="/">
               <img src={logo} alt="logo" className='w-[180px] object-contain' />
             </Link>
@@ -15,6 +15,15 @@ import { Home, CreatePost } from './pages'
             
             <Link to="/create-post" className='font-inter font-medium bg-gradient-to-r from-sky-500 via-50% to-indigo-500 to-55% text-white px-4 py-2 rounded-md'>Create</Link>
           </header>
+
+          <div className='sticky top-0'>
+          <Link to="/">
+              <img src={logo} alt="logo" className='w-[180px] object-contain' />
+            </Link>
+
+            
+            <Link to="/create-post" className='font-inter font-medium bg-gradient-to-r from-sky-500 via-50% to-indigo-500 to-55% text-white px-4 py-2 rounded-md'>Create</Link>
+          </div>
           <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
             <Routes>
               <Route path="/" element={<Home />}/>
